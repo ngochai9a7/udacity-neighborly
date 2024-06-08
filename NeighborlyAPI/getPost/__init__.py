127863e5181421e85c462c9bcf8b908f4f82721c
Hai <ngochai9a7@gmail.com>
2024-06-08 13:27:51 +0700

    first commit

---
+++ NeighborlyAPI/getPost/__init__.py
@@ -0,0 +1,27 @@
+import azure.functions as func
+import pymongo
+import json
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    id = req.params.get('id')
+
+    if id:
+        try:
+            url = "mongodb://neighborlycosmosdb:4CavwWB9VDXIPgkoV42Yd1rQuFyDD4y84YSAT5giKBn2N8PFQ9u8oBbHdIZubqz077FHe5TkkU1NACDbLobKZg==@neighborlycosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborlycosmosdb@"  # TODO: Update with appropriate MongoDB connection information
+            client = pymongo.MongoClient(url)
+            database = client['neighborly']
+            collection = database['posts']
+
+            query = {'_id': ObjectId(id)}
+            result = collection.find_one(query)
+            result = dumps(result)
+
+            return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+        except:
+            return func.HttpResponse("Database connection error.", status_code=500)
+
+    else:
+        return func.HttpResponse("Please pass an id parameter in the query string.", status_code=400)
